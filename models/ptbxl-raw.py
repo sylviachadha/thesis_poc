9411a48c657247d3014e45ebedfacacfa9973c69
Sylvia Chadha <sylviachadha@gmail.com>
2021-06-10 16:08:27 +0700

    ptbxl-raw

---
+++ models/ptbxl-raw.py
@@ -0,0 +1,73 @@
+# Aim - ptb-xl - deep learning ANN Classification
+# Normal vs LVH
+# --------------------------------------------------#
+# STEP 1 - Load data   (# AS PAPER)
+# --------------------------------------------------#
+
+import pandas as pd
+import numpy as np
+import wfdb
+import ast
+from sklearn.metrics import f1_score, confusion_matrix, accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+
+
+def load_raw_data(df, sampling_rate, path):
+    if sampling_rate == 100:
+        data = [wfdb.rdsamp(path + f) for f in df.filename_lr]
+    else:
+        data = [wfdb.rdsamp(path + f) for f in df.filename_hr]
+    data = np.array([signal for signal, meta in data])
+    return data
+
+
+path = '/Users/sylvia/Desktop/Thesis/Dataset/ptb-xl/'
+sampling_rate = 100
+
+# load and convert annotation data
+Y = pd.read_csv(path + 'ptbxl_database.csv', index_col='ecg_id')
+Y.scp_codes = Y.scp_codes.apply(lambda x: ast.literal_eval(x))
+
+# Load raw signal data
+X = load_raw_data(Y, sampling_rate, path)
+
+# Load scp_statements.csv for diagnostic aggregation
+agg_df = pd.read_csv(path + 'scp_statements.csv', index_col=0)
+agg_df = agg_df[agg_df.diagnostic == 1]
+
+
+def aggregate_diagnostic(y_dic):
+    tmp = []
+    for key in y_dic.keys():
+        if key in agg_df.index:
+            tmp.append(agg_df.loc[key].diagnostic_class)
+    return list(set(tmp))
+
+
+# Apply diagnostic superclass
+Y['diagnostic_superclass'] = Y.scp_codes.apply(aggregate_diagnostic)
+
+# Select only combination of NORM and SR as NORM
+
+def aggregate_diagnostic1(y_dic):
+    tmp = []
+    for key in y_dic.keys():
+        if key in agg_df.index:
+            tmp.append(agg_df.loc[key].diagnostic_subclass)
+    return list(set(tmp))
+
+
+# Apply diagnostic subclass
+Y['diagnostic_subclass'] = Y.scp_codes.apply(aggregate_diagnostic1)
+
+print(Y['diagnostic_superclass'])
+
+
+# Export to Excel
+Y.to_csv('/Users/sylvia/Desktop/Thesis/Dataset/Export/Y.csv')
+
+
+
+
+
